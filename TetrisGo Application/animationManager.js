7a8688541c92ecff07a334f419e356ac36bbc253
dostee-hemin <dostee.hemin@gmail.com>
2022-05-09 23:09:30 +0300

    Updated Transition Animation

--- TetrisGo Application/animationManager.js
+++ TetrisGo Application/animationManager.js
@@ -48,11 +48,11 @@ let rainEffect;                     // The particle system for creating a rain e
 /*-------------------- Setup -------------------*/
 function setupAnimationPart() {
     // Assign the columns and rows according to the width and height of the canvas
-    transCols = floor(width / transScl) + transExtra;
+    transCols = floor(width / transScl);
     transRows = floor(height / transScl) + transExtra;
 
     // Initialize all of the prompt offsets to 0
-    for(var i=0; i<prompt.length; i++) promptOffsets[i] = 0;
+    for (var i = 0; i < prompt.length; i++) promptOffsets[i] = 0;
 
     // Initialize the rain effect manager
     rainEffect = new RainEffect();
@@ -78,22 +78,22 @@ class RainEffect {
 
     // Loop through every piece in the array and display it
     display() {
-        for (const piece in this.fallingPieces) piece.display();
+        for (var i=0; i<this.fallingPieces.length; i++) this.fallingPieces[i].display();
     }
 
     update() {
         // Every frame, there's a random chance of adding a new falling piece
-        if(random(1) < 0.05) this.fallingPieces.push(new FallingPiece());
+        if (random(1) < 0.05) this.fallingPieces.push(new FallingPiece());
 
         // Loop through all the falling pieces
-        for(var i=this.fallingPieces.length-1; i>=0; i--) {
+        for (var i = this.fallingPieces.length - 1; i >= 0; i--) {
             var piece = this.fallingPieces[i];
 
             // Update the current piece
             piece.update();
 
             // If the piece has reached the bottom of the screen, remove it from the array
-            if(piece.isFinished()) this.fallingPieces.splice(i, 1);
+            if (piece.isFinished()) this.fallingPieces.splice(i, 1);
         }
     }
 }
@@ -108,7 +108,7 @@ class FallingPiece {
         // Give the piece a random rotation, type, and speed
         this.rotation = floor(random(4));
         this.type = floor(random(7));
-        this.speed = random(3,7);
+        this.speed = random(3, 7);
 
         // Set the frame this piece is created to the current frame
         this.startingFrame = frameCount;
@@ -120,17 +120,17 @@ class FallingPiece {
     display() {
         // Display the piece shape with the correct rotation
         push();
-        translate(this.x,this.y);
-        for(var i=0; i<this.rotation; i++) rotate(HALF_PI);
+        translate(this.x, this.y);
+        for (var i = 0; i < this.rotation; i++) rotate(HALF_PI);
         noStroke();
-        colorFromType(this.type+1);
-        drawPieceShape(this.type,0,0,this.size);
+        colorFromType(this.type + 1);
+        drawPieceShape(this.type, 0, 0, this.size);
         pop();
     }
 
     update() {
         // Every couple of frames, rotate the piece
-        if((frameCount - this.startingFrame) % 40 == 0) (this.rotation++)%4;
+        if ((frameCount - this.startingFrame) % 40 == 0) (this.rotation++) % 4;
 
         // Move the piece down by the speed
         this.y += this.speed;
@@ -138,7 +138,7 @@ class FallingPiece {
 
     // Returns true if the piece has reached the bottom of the screen
     isFinished() {
-        return this.y > height+200;
+        return this.y > height + 200;
     }
 }
 
@@ -175,16 +175,16 @@ function makeTransition(scene) {
     // Set the scene we want to transition into using the given scene
     destinationScene = scene;
 
-    // Represents the number of times we have tried to add a new piece to the grid
-    var currentIteration = 0;
-
+    
     // Continuously add new pieces to the grid (we will break out of the loop later)
     while (true) {
-        // Pick a random column in the grid
-        var currentColumn = floor(random(transCols));
+        // Variables to find the best location to drop a piece
+        var recordCost = 1000000;
+        var bestRotation = 0;
+        var bestPiece = [];
 
         // Pick a random piece type
-        var type = currentIteration % 7;
+        var type = floor(random(7));
 
         // Create the piece from the type at the top of the grid
         var currentPiece = [];
@@ -193,10 +193,6 @@ function makeTransition(scene) {
 
         // Loop through every possible rotation of the current piece
         for (let currentRotation = 0; currentRotation < 4; currentRotation++) {
-            // If the current piece is an I piece and it is vertical, don't add this piece.
-            // This is because this piece will always be placed, but will end up creating pillars.
-            if (type == 1 && currentRotation % 2 == 0) break;
-
             // Duplicate the current tetromino to represent the current rotation
             let testPiece = duplicateTetromino(currentPiece);
 
@@ -256,131 +252,158 @@ function makeTransition(scene) {
             // Calculate how wide the tetromino is based on the min and max values
             let pieceWidth = rightmostBox - leftmostBox;
 
-            // Move over the piece to the current simulation column
-            for (let j = 0; j < currentColumn && j < transCols - pieceWidth; j++) {
-                for (let k=0; k<4; k++) testPiece[k][0]++;
-            }
+            for (let currentColumn = 0; currentColumn < transCols - pieceWidth; currentColumn++) {
+                // Represents how bad or good the current placement is
+                var cost = 0;
 
-            /* Calculate where the piece will land if we let it drop */
-            // Continuously move the piece down
-            while (true) {
-                for (var i=0; i<4; i++) testPiece[i][1]++;
+                // Copy the piece to a new variable so we can manipulate it
+                var copyPiece = duplicateTetromino(testPiece);
 
-                // If the piece hits the bottom of the grid, leave the loop
-                if (goesOut(testPiece)) break;
-                if (lands(testPiece)) break;
-            }
-            // This boolean determines whether or not the current rotation is valid (i.e. if the piece isn't outside the grid from the rotation)
-            let isRotationValid = true;
-            // Move the piece one unit back up (to fix overshooting)
-            for (let i = 0; i < 4; i++) {
-                testPiece[i][1]--;
-                // If the box is outside the grid, the current rotation is not valid so leave the loop
-                if (testPiece[i][0] < 0 || testPiece[i][0] > transCols - 1 || testPiece[i][1] < transExtra - 4) {
-                    isRotationValid = false;
-                    break;
+                // Move over the piece to the current simulation column
+                for (let j = 0; j < currentColumn; j++) {
+                    for (let k = 0; k < 4; k++) copyPiece[k][0]++;
                 }
-            }
-            // If the rotation is invalid, move on to the next rotation
-            if (!isRotationValid) continue;
 
-            // This check is used to see if the current placement is above the screen (i.e. unnecessary to place)
-            var isBelowScreen = false;
-            for (var b = 0; b < 4; b++) {
-                if (testPiece[b][1] >= transExtra) {
-                    isBelowScreen = true;
+                /* Calculate where the piece will land if we let it drop */
+                // Continuously move the piece down
+                while (true) {
+                    for (var i = 0; i < 4; i++) copyPiece[i][1]++;
+
+                    // If the piece hits the bottom of the grid, leave the loop
+                    if (goesOut(copyPiece)) break;
+                    if (lands(copyPiece)) break;
                 }
-            }
-            if (!isBelowScreen) continue;
+                // This boolean determines whether or not the current rotation is valid (i.e. if the piece isn't outside the grid from the rotation)
+                let isRotationValid = true;
+                // Move the piece one unit back up (to fix overshooting)
+                for (let i = 0; i < 4; i++) {
+                    copyPiece[i][1]--;
 
-            // Represents a copied version of the grid that we 
-            // use to check if the current piece is valid in this position
-            var copiedGrid = [];
-
-            // Copy the grid over to the new version
-            for (let i = 0; i < transCols; i++) {
-                copiedGrid[i] = [];
-                for (let j = 0; j < transRows; j++) {
-                    copiedGrid[i][j] = transGrid[i][j];
-                }
-            }
-
-            // Set the grid at the current piece's location to be -1 (just so we can understand the layout of the stack later)
-            for (let j = 0; j < 4; j++) {
-                copiedGrid[testPiece[j][0]][testPiece[j][1]] = -1;
-            }
-
-            /* Look for holes that the tetromino creates */
-            // Loop through every column in the grid
-            var canBePlaced = true;
-            for (let j = 0; j < transCols; j++) {
-                // This variable represents the top of the stack at the current column
-                let topI = 0;
-                // Find the top of the stack by continuously moving down
-                while (copiedGrid[j][topI] == 0) topI++;
-
-                // Loop through every row underneath the top of the stack
-                for (let i = topI; i < transRows; i++) {
-                    // If the current grid element is empty, that means it's a hole, which also means
-                    // we can not place the current piece here, so update the boolean and leave the loop
-                    if (copiedGrid[j][i] == 0) {
-                        canBePlaced = false;
+                    // If the box is outside the grid, the current rotation is not valid so leave the loop
+                    if (copyPiece[i][0] < 0 || copyPiece[i][0] > transCols - 1 || copyPiece[i][1] < transExtra - 4) {
+                        isRotationValid = false;
                         break;
                     }
+
+                    // Increase the cost based on the height of the box
+                    cost += (transRows-copyPiece[i][1])*costWeights[0];
                 }
-            }
+                // If the rotation is invalid, move on to the next rotation
+                if (!isRotationValid) continue;
 
-            // If the current piece does not create any holes, we can add it to the transition grid
-            if (canBePlaced) {
-                // Add it to the list of pieces
-                transitionPieces.push(new TransitionPiece(testPiece, type, currentRotation));
+                // Represents a copied version of the grid that we 
+                // use to check if the current piece is valid in this position
+                var copiedGrid = [];
 
-                // Update the grid
-                for (var i = 0; i < 4; i++) {
-                    transGrid[testPiece[i][0]][testPiece[i][1]] = type + 1;
+                // Copy the grid over to the new version
+                for (let i = 0; i < transCols; i++) {
+                    copiedGrid[i] = [];
+                    for (let j = 0; j < transRows; j++) {
+                        copiedGrid[i][j] = transGrid[i][j];
+                    }
                 }
 
-                // At this point, the new piece might have created a column that only a vertical I piece can fill,
-                // so look for those columns and fill them.
+                // Set the grid at the current piece's location to be -1 (just so we can understand the layout of the stack later)
+                for (let j = 0; j < 4; j++) {
+                    copiedGrid[copyPiece[j][0]][copyPiece[j][1]] = -1;
+                }
+
+                /* Look for holes that the tetromino creates */
                 // Loop through every column in the grid
-                for (let j = 0; j < transCols - 1; j++) {
+                var canBePlaced = true;
+                for (let j = 0; j < transCols; j++) {
                     // This variable represents the top of the stack at the current column
                     let topI = 0;
                     // Find the top of the stack by continuously moving down
-                    let columnCount = 0;
-                    while (transGrid[j][topI] == 0) {
-                        topI++;
+                    while (copiedGrid[j][topI] == 0) topI++;
 
-                        // At each iteration, check if a column exists to the left and right of the current column
-                        if (j == 0) {
-                            if (transGrid[j + 1][topI] != 0) columnCount++;
-                        } else if (j == transCols - 1) {
-                            if (transGrid[j - 1][topI] != 0) columnCount++;
-                        } else {
-                            if (transGrid[j - 1][topI] != 0 && transGrid[j + 1][topI] != 0) columnCount++;
+                    // Loop through every row underneath the top of the stack
+                    for (let i = topI; i < transRows; i++) {
+                        // If the current grid element is empty, that means it's a hole, which also means
+                        // we can not place the current piece here, so update the boolean and leave the loop
+                        if (copiedGrid[j][i] == 0) {
+                            canBePlaced = false;
+                            break;
                         }
                     }
-                    // If a column exists that's too tall, add a vertical I piece in that location
-                    if (columnCount > 3) {
-                        // Update the grid
-                        topI--;
-                        for (var b=0; b<4; b++) transGrid[j][topI - b] = 2;
+                }
 
-                        // Add it to the list of pieces
-                        transitionPieces.push(new TransitionPiece([[j, topI - 1], [j, topI], [j, topI - 2], [j, topI - 3]], 1, 3));
+                // If the piece in the current column makes a hole, move on to the next column
+                if(!canBePlaced) continue;
+
+                // At this point, the current placement does not create any holes,
+                // so now we can figure out if it's actually a good place to put the piece
+
+                /* Calculate how many lines the current piece will clear */
+                // This variable keeps track of the number of lines the current piece will clear
+                let numberOfClears = 0;
+                // Loop through every row in the grid
+                for (let i = 0; i < transRows; i++) {
+                    // This counter keeps track of the number of boxes in the current row
+                    let counter = 0;
+                    // Loop through all the columns in this row; if there's a box, add 1 to the counter
+                    for (let j = 0; j < transCols; j++) {
+                        if (copiedGrid[j][i] != 0) counter++;
+                    }
+                    // If all boxes in the row are full (i.e. the line is completed), move all the previous rows down
+                    if (counter == transCols) {
+                    for (let j = 0; j < transCols; j++) {
+                        for (let I = i - 1; I >= -1; I--) {
+                            // This if fixes the issue when moving down the top row of the grid
+                            if(I == -1) copiedGrid[j][0] = 0;
+                            else copiedGrid[j][I + 1] = copiedGrid[j][I];
+                        }
+                    }
+                    numberOfClears++;
                     }
                 }
-                break;
+
+                // Increase the cost based on the number of lines that will be cleared
+                cost += numberOfClears*costWeights[2];
+
+                /* Calculate how rough or flat the stack will be with the current piece */
+                // These two variables represent the change in rows from one column to the next
+                let currentHeight;
+                let previousHeight;
+                // Loop through every column
+                for(let j=0; j<transCols-1; j++) {
+                    // Set the previous height to the current height
+                    previousHeight = currentHeight;
+
+                    // This variable represents the top of the stack at the current column
+                    let topI=0;
+                    // Find the top of the stack by continuously moving down
+                    while(copiedGrid[j][topI] == 0) topI++;
+                    // The current height is the top of the stack we just found
+                    currentHeight = topI;
+
+                    // Only calculate the cost when we have a difference between 2 columns
+                    if(j > 0) cost += abs(currentHeight-previousHeight)*costWeights[5];
+                }
+
+                // If the current move we just calculated beats the best move, record all the values so we can use it later
+                if (cost < recordCost) {
+                    recordCost = cost;
+                    bestRotation = currentRotation;
+                    bestPiece = copyPiece;
+                }
             }
         }
 
-        currentIteration++;
 
+        // At this point, we have found the best place to put the current piece, so add it to the list of pieces
+        transitionPieces.push(new TransitionPiece(bestPiece, type, bestRotation));
+
+        // Update the grid
+        for (var i = 0; i < 4; i++) {
+            transGrid[bestPiece[i][0]][bestPiece[i][1]] = type + 1;
+        }
+        
         // Check to see if we have finished filling the grid
         var canContinue = false;
         // Loop through all the columns of the top-most row.
         // If all the cells are filled, we are finished, so we can break out of the loop
-        for (var j = 0; j < transCols - transExtra; j++) {
+        for (var j = 0; j < transCols; j++) {
             if (transGrid[j][transExtra] == 0) {
                 canContinue = true;
                 break;
@@ -389,6 +412,7 @@ function makeTransition(scene) {
         if (!canContinue) break;
     }
 }
+
 
 // Similar function to outOfBounds(), but for the transition grid
 function goesOut(tetromino) {
@@ -422,7 +446,7 @@ function lands(tetromino) {
 function showTransition() {
     // If we aren't currently transitioning, then leave the loop
     if (!isTransitioning) return;
-    
+
     // Keep showing more and more pieces
     currentTransitionPiece++;
 
@@ -450,9 +474,10 @@ function showTransition() {
 class TransitionPiece {
     constructor(boxes, type, rotation) {
         // Stores the x-y coordinates of the center block of the current piece
-        this.center = { 
-            x: (boxes[0][0]+0.5) * transScl, 
-            y: (boxes[0][1] - transExtra+0.5) * transScl };
+        this.center = {
+            x: (boxes[0][0] + 0.5) * transScl,
+            y: (boxes[0][1] - transExtra + 0.5) * transScl
+        };
 
         this.type = type;
         this.rotation = rotation;
